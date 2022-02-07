c32aafd78a3f8861af98b2675768768c586940b8
Thimoty Barbieri <thimoty@thimoty.it>
2022-02-07 23:10:16 +0100

    custom metrics added

--- microservice-map-info/Services/DistanceInfoService.cs
+++ microservice-map-info/Services/DistanceInfoService.cs
@@ -1,11 +1,17 @@
 ﻿using GoogleMapInfo;
 using Grpc.Core;
 using microservice_map_info.Protos;
+using Prometheus;
 
 namespace microservice_map_info.Services
 {
     public class DistanceInfoService : DistanceInfo.DistanceInfoBase
     {
+
+        // Aggiungi metriche custom
+        private static readonly Counter googleApiCount = Metrics.CreateCounter("google_api_calls_total", "Number of times Google geolocation api is called.");
+        private static readonly Counter googleApiLocations = Metrics.CreateCounter("google_api_locations", "Google Maps from and to locations.");
+
         private readonly ILogger<DistanceInfoService> _logger;
         private readonly GoogleDistanceApi _googleDistanceApi;
         public DistanceInfoService(ILogger<DistanceInfoService> logger, GoogleDistanceApi googleDistanceApi)
@@ -17,6 +23,11 @@ namespace microservice_map_info.Services
         public override async Task<DistanceData> GetDistance(Cities cities, ServerCallContext context)
         {
             var totalMiles = "0";
+
+            // Issue metrics
+            googleApiCount.Inc();
+            googleApiLocations.WithLabels(cities.OriginCity, cities.DestinationCity).Inc();
+
             var distanceData = await _googleDistanceApi.GetMapDistance(cities.OriginCity, cities.DestinationCity);
             foreach (var distanceDataRow in distanceData[0].rows)
             {

acf7f059f0f33fa51d200730baab03fa547546fa
Thimoty Barbieri <thimoty@thimoty.it>
2022-02-07 23:03:50 +0100

    added metrics infrastructure

---
+++ docker-build.sh
@@ -0,0 +1,1 @@
+ docker build -f microservice-map-info/Dockerfile --target final -t microservicemapinfo:dev .
--- microservice-map-info/Program.cs
+++ microservice-map-info/Program.cs
@@ -1,13 +1,14 @@
 using GoogleMapInfo;
 using microservice_map_info.Services;
 using Serilog;
+using Prometheus;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add Serilog support
 builder.Host.UseSerilog((ctx, lc) => lc
     .WriteTo.Console()
-    .WriteTo.Seq("http://localhost:5341")
+   // .WriteTo.Seq("http://localhost:5341")
     .ReadFrom.Configuration(ctx.Configuration));
 
 Serilog.Debugging.SelfLog.Enable(Console.Error);
@@ -43,9 +44,14 @@ app.MapControllers();
 
 app.UseRouting();
 
+// Expose promotheus metrics
+app.UseHttpMetrics();
+
+
 app.UseEndpoints(endpoints =>
 {
     endpoints.MapGrpcService<DistanceInfoService>();
+    endpoints.MapMetrics(); // endpoint per Prometheus
 });
 
 app.Run();
